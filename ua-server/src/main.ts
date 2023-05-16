f0087a76df17bb0f29d90779575de03bc9ed5bec
Mohammad Mohammad <mohammad.mohammad@codewerk.de>
2023-05-16 14:42:41 +0200

    in progress: mqtt client

--- ua-server/src/main.ts
+++ ua-server/src/main.ts
@@ -2,9 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule as OpcuaModule } from './opcua.module';
 import { OpcuaServerService } from './opcua-server.service';
 import { catchError, switchMap } from 'rxjs';
+import { MqttService } from './mqtt.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(OpcuaModule);
+  const uaServer = app.get(OpcuaServerService);
+  const mqttService = app.get(MqttService);
+
+  uaServer
+    .initialize()
+    .pipe(
+      switchMap(() => uaServer.start()),
+      catchError((error) => {
+        console.log(error);
+        return app.close();
+      }),
+    )
+    .subscribe();
+
+    await mqttService.connectClient('mqtt://localhost:1883');
+    await mqttService.subscribe('Hello/World');
+    await mqttService.publish('Hello/World', 'Bye World');
+    await mqttService.disconnectClient();
 
   process.on('SIGINT', async () => await app.close());
   process.on('SIGTERM', async () => await app.close());
--- ua-server/src/mqtt.service.ts
+++ ua-server/src/mqtt.service.ts
@@ -1,25 +1,39 @@
 import { Injectable, ConsoleLogger  } from "@nestjs/common";
 import { OpcuaServerService } from "./opcua-server.service";
-import { AsyncMqttClient, connectAsync } from "async-mqtt";
+import { AsyncMqttClient, IClientSubscribeOptions, ISubscriptionMap, connectAsync } from "async-mqtt";
 
 @Injectable()
 export class MqttService {
     private _client: AsyncMqttClient;
     constructor(private readonly _logger: ConsoleLogger, private _opcuaService: OpcuaServerService) {
         this._logger.setContext(MqttService.name);
-        this._logger.log('I AM RUNNING!');
-        this.connectClient('mqtt://test.mosquitto.org');
-        this._initializeListeners();
+        //this._initializeListeners();
     }
 
    async connectClient(brokerURL: string) {
        this._client = await connectAsync(brokerURL);
     }
 
-    private _initializeListeners(){
+    async disconnectClient(force: boolean = false){
+        this._client.end(force);
+    }
+
+    async publish(topic: string, message: any){
         if(this._client.connected){
-            console.log('Successfully Connected!');
+            try {
+                await this._client.publish(topic, message);
+            } catch (e){
+                this._logger.log(e.stack);
+            }
         }
     }
 
+    async subscribe(topic: string, options?: IClientSubscribeOptions) {
+        if(this._client.connected){
+            let grants = await this._client.subscribe(topic, options);
+            grants.forEach(grant => {
+                console.log(grant);
+            })
+        }
+    }
 }
